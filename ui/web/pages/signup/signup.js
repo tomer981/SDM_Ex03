e73f50156a9d027b4ef7171328645264c17c48df
tomer981 <tomer981@gmail.com>
2020-10-24 20:03:15 +0300

    start fix names

--- ui/web/pages/signup/signup.js
+++ ui/web/pages/signup/signup.js
@@ -1,12 +1,41 @@
-import {ThingA, ThingB, ThingC} from 'lib/things';
-
-
 isSessionExist({action: "isSessionExist",userNotFoundPage: null, userFoundPage: "../zones/zones.html"});
-$("#submitForm").ready(function() {
-    $(':input[type="submit"]').prop('disabled', true);
-    $('input[type="text"]').keyup(function() {
-        if($(this).val() !== '') {
-            $(':input[type="submit"]').prop('disabled', false);
+
+function submitButtonValidateParameters() {
+    $("#submitFormButton").ready(function () {
+        if ($("#user-name").val() !== "") {
+            if ($("#user-position").find("option:selected").attr("value") === "customer") {
+                $("#add-file").prop('disabled', true);
+                $("#add-file").val("");
+                $("#submitFormButton").prop("disabled", false);
+            } else if ($("#user-position").find("option:selected").attr("value") === "manager") {
+                $("#add-file").prop('disabled', false);
+                if ($("#add-file").val() !== "") {
+                    $("#submitFormButton").prop("disabled", false);
+                } else {
+                    $("#submitFormButton").prop("disabled", true);
+                }
+            }
+        } else {
+            $("#add-file").prop('disabled', true);
+            $("#add-file").val("");
+            $("#submitFormButton").prop("disabled", true);
         }
-    });
-});
+    })
+}
+
+// $("#submitForm").on("submit", function (e) {
+//     var form = this;
+//     $.ajax({
+//         url: this.action,
+//         type: this.method,
+//         data: $(this).serialize(),
+//         success: function (nextUrl) {
+//             window.location.reload();
+//         },
+//         error: function (){
+//
+//         }
+//     });
+//
+//     return false;
+// });
