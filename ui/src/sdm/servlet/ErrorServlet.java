faef8c352d3354e342a0c62af11af7c5146d538b
Aviad BD <aviad.bendov@gmail.com>
2020-10-24 18:05:38 +0300

    More complex way to send error

--- ui/src/sdm/servlet/ErrorServlet.java
+++ ui/src/sdm/servlet/ErrorServlet.java
@@ -10,13 +10,16 @@ import java.io.IOException;
 @WebServlet("/ErrorHandler")
 public class ErrorServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Exception exception = (Exception)req.getAttribute("javax.servlet.error.exception");
+        Exception exception = (Exception) req.getAttribute("javax.servlet.error.exception");
         // Integer statusCode = (Integer)req.getAttribute("javax.servlet.error.status_code");
         // String servletName = (String)req.getAttribute("javax.servlet.error.servlet_name");
-        String requestUri = (String)req.getAttribute("javax.servlet.error.request_uri");
+        String requestUri = (String) req.getAttribute("javax.servlet.error.request_uri");
 
-        // TODO: Redirect anywhere you want with the exception in the previous variables
-        resp.setHeader("Error", exception.getMessage());
-        resp.sendRedirect(requestUri);
+        String errorParameter = "errorMessage=" + exception.getMessage();
+        String redirectedUri = requestUri.contains("?")
+                ? "&" + errorParameter
+                : "?" + errorParameter;
+
+        resp.sendRedirect(redirectedUri);
     }
 }
