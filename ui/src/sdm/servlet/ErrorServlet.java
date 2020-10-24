c20085784b68d9a662f2046232d36205c47330b4
Aviad BD <aviad.bendov@gmail.com>
2020-10-24 17:22:00 +0300

    Added exception handling

---
+++ ui/src/sdm/servlet/ErrorServlet.java
@@ -0,0 +1,20 @@
+package sdm.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ErrorServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Exception exception = (Exception)req.getAttribute("javax.servlet.error.exception");
+        // Integer statusCode = (Integer)req.getAttribute("javax.servlet.error.status_code");
+        // String servletName = (String)req.getAttribute("javax.servlet.error.servlet_name");
+        String requestUri = (String)req.getAttribute("javax.servlet.error.request_uri");
+
+        // TODO: Redirect anywhere you want with the exception in the previous variables
+        resp.setHeader("Error", exception.getMessage());
+        resp.sendRedirect(requestUri);
+    }
+}
