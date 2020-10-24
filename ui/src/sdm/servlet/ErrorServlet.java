ddf47050a737fe00775d8481d2b13604c6541e7b
Aviad BD <aviad.bendov@gmail.com>
2020-10-24 17:28:07 +0300

    Added servlet tag

--- ui/src/sdm/servlet/ErrorServlet.java
+++ ui/src/sdm/servlet/ErrorServlet.java
@@ -1,11 +1,13 @@
 package sdm.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/ErrorHandler")
 public class ErrorServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Exception exception = (Exception)req.getAttribute("javax.servlet.error.exception");
