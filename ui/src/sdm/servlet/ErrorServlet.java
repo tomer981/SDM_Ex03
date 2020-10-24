078637873973a022ed29854846ae70c207ec6039
Aviad BD <aviad.bendov@gmail.com>
2020-10-24 18:10:17 +0300

    opt import

--- ui/src/sdm/servlet/ErrorServlet.java
+++ ui/src/sdm/servlet/ErrorServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 @WebServlet("/ErrorHandler")
 public class ErrorServlet extends HttpServlet {
