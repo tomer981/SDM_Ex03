4694f867a369186a7d548ff77754dd4971b44236
Aviad BD <aviad.bendov@gmail.com>
2020-10-30 15:11:35 +0200

    Added mechanism for updates client-server side using ajax and intervals

--- ui/src/sdm/servlet/UpdatesServlet.java
+++ ui/src/sdm/servlet/UpdatesServlet.java
@@ -1,21 +1,38 @@
 package sdm.servlet;
 
 
+import com.google.gson.JsonObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "UpdatesServlet", urlPatterns = {"/UpdatesServlet", "/updates/StoresServlet"})
+@WebServlet(name = "UpdatesServlet", urlPatterns = {"/UpdatesServlet"})
 public class UpdatesServlet extends HttpServlet {
+    int stopAfter = 3;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            Thread.sleep(5000);
+        try (PrintWriter out = resp.getWriter()) {
 
-            resp.getWriter().write("{'text': 'hello'");
+            System.out.println("Sleep started");
+            Thread.sleep(500);
+            System.out.println("Sleep ended");
+
+            JsonObject json = new JsonObject();
+            json.addProperty("text", "hello");
+
+            if (stopAfter > 0) {
+                stopAfter--;
+            } else {
+                json.addProperty("stopChecking", true);
+            }
+
+            out.println(json);
         } catch (InterruptedException e) {
             throw new RuntimeException("Who woke me up");
         }
--- ui/web/common/check-for-updates.js
+++ ui/web/common/check-for-updates.js
@@ -1,12 +1,36 @@
-function checkForUpdates() {
-    console.log("Checking for updates")
-    $.ajax({
-        method: "GET",
-        url: CHECK_FOR_UPDATES,
-        data: {},
-        success: function (updates) {
-            console.log("~~~~~ Got Updates! ~~~~~")
-            console.log(updates)
-        }
-    })
+const interval = 500
+let updatesCheckerHandler = null;
+let isChecking = false
+
+function startCheckForUpdates(cb) {
+    if (updatesCheckerHandler === null) {
+        updatesCheckerHandler = setInterval(() => {
+            if (isChecking) return
+            isChecking = true
+            $.ajax({
+                method: "Get",
+                url: CHECK_FOR_UPDATES,
+                data: {},
+                success: function (updatesString) {
+                    let updates = null
+                    try {
+                        updates = JSON.parse(updatesString)
+                        cb(updates)
+                        isChecking = false
+
+                        if (updates.stopChecking !== undefined && updates.stopChecking) {
+                            clearInterval(updatesCheckerHandler)
+                            updatesCheckerHandler = null;
+                        }
+                    } catch(err) {
+                        console.log("Could not parse updates")
+                        console.log(err)
+
+                        isChecking = false
+                    }
+
+                }
+            })
+        }, interval);
+    }
 }
